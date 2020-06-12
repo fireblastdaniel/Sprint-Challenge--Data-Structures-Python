28f8961f5602071c8d8e135a19d92318b1dff2b2
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-06-12 08:56:58 -0700

    names passes tests, stretch goal attempted

--- names/names.py
+++ names/names.py
@@ -1,4 +1,5 @@
 import time
+from bst import TreeNode
 
 start_time = time.time()
 
@@ -12,11 +13,13 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+names_tree = TreeNode(names_1[0])
+for name in names_1[1:]:
+    names_tree.insert(name)
+
+for name in names_2:
+    if names_tree.contains(name):
+        duplicates.append(name)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -26,3 +29,24 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# start_time = time.time()
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# duplicates = []
+# names_set = set(names_1)
+
+# for name in names_2:
+#     if name in names_set:
+#         duplicates.append(name)
+
+# end_time = time.time()
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
